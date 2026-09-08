d436f8a47dbd0ab84d3b31b128c4633e3b658ed1
sl91994 <112153975+sl91994@users.noreply.github.com>
2026-09-08 19:36:07 +0900

    初期のCLI構造と各種モジュールを追加 (#1)
    * build: clippy, rustfmtの設定を追加
    
    * build: anyhowを追加
    
    * feat(model): 空のモデルを追加
    
    * feat(commands): 空のサブコマンドを追加
    
    * feat(cli): cliの最小構成を追加
    
    * feat(template): templateモジュールを追加
    
    * feat(store): discover_root関数を追加
    
    * feat(output): outputモジュールを追加
    
    * feat(commands): commandsモジュールを追加し，dispatch関数を実装
    
    * fix(commands): run関数の引数の綴りミスを修正
    
    * fix(rustfmt): 不要な設定を削除
    
    * feat(commands): 各コマンドスタブの未実装メッセージを追加
    
    * build(ci): CIのrustバージョンをrust-toolchain.tomlと同様のものに固定
    
    * fix: rustfmtの空ファイルエラーを回避

---
+++ src/commands/init.rs
@@ -0,0 +1,3 @@
+pub fn run() -> anyhow::Result<()> {
+    anyhow::bail!("todoke run is not implemented yet")
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+mod cli;
+mod commands;
+mod model;
+mod output;
+mod store;
+mod template;
+
+use crate::cli::Args;
+use clap::Parser;
+
+fn main() -> anyhow::Result<()> {
+    let args = Args::parse();
+    commands::dispatch(args)
 }
